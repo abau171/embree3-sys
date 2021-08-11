537188081d5c8dfd937349066d0412580cda7a8b
Andrew Bauer <abau171@gmail.com>
2021-08-11 13:38:19 -0400

    Added backup Embree headers which are built during CI or to produce backup documentation.

--- build.rs
+++ build.rs
@@ -1,27 +1,63 @@
 use std::env;
 use std::path::PathBuf;
 
+// If any of these environment variables are set and are not "0", then we use
+// some backup headers and the Embree lib is not linked. This is useful for
+// providing documentation and running CI without requiring an Embree
+// installation.
+const USE_BACKUP_HEADERS_ENV_VARS: &[&str] = &[
+    "DOCS_RS",
+    "CI",
+];
+
 fn main() {
+
     // Based on https://rust-lang.github.io/rust-bindgen/tutorial-0.html
 
-    // If EMBREE_DIR is set, add it as a linker search path.
-    if let Ok(embree_dir_str) = env::var("EMBREE_DIR") {
-        let embree_lib_dir = PathBuf::from(embree_dir_str).join("lib");
-        println!(
-            "cargo:rustc-link-search=native={}",
-            embree_lib_dir.display()
-        );
-        println!("cargo:rerun-if-env-changed=EMBREE_DIR");
+    // Determine if we should use the backup headers (if any of the environment
+    // variables is set and not "0").
+    let mut use_backup_headers = false;
+    for env_var in USE_BACKUP_HEADERS_ENV_VARS {
+        println!("cargo:rerun-if-env-changed={}", env_var);
+        use_backup_headers =
+            use_backup_headers
+            || env::var_os(env_var).filter(|v| v != "0").is_some();
     }
 
-    // Link to the Embree lib.
-    println!("cargo:rustc-link-lib=embree3");
+    // Select a wrapper header and link to the Embree lib.
+    let wrapper_path;
+    if !use_backup_headers {
+
+        wrapper_path = "wrapper.h";
+
+        // If EMBREE_DIR is set, add it as a linker search path.
+        println!("cargo:rerun-if-env-changed=EMBREE_DIR");
+        if let Ok(embree_dir_str) = env::var("EMBREE_DIR") {
+            let embree_lib_dir = PathBuf::from(embree_dir_str).join("lib");
+            println!(
+                "cargo:rustc-link-search=native={}",
+                embree_lib_dir.display()
+            );
+        }
+
+        // Link to the Embree lib.
+        println!("cargo:rustc-link-lib=embree3");
+
+    } else {
+
+        wrapper_path = "backup_embree_headers/wrapper.h";
+
+        // We are using the backup headers, so don't attempt to link to the
+        // Embree lib.
+
+    }
 
     // If wrapper.h changes, rebuild the crate.
-    println!("cargo:rerun-if-changed=wrapper.h");
+    println!("cargo:rerun-if-changed={}", wrapper_path);
 
+    // Generate the bindings.
     let bindings = bindgen::Builder::default()
-        .header("wrapper.h")
+        .header(wrapper_path)
         // If headers included in wrapper.h change, rebuild the crate.
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .generate_comments(false)
@@ -67,7 +103,7 @@ fn main() {
             "pub type __ssize_t = isize",
         );
 
-    // Write the processed bindings
+    // Write the processed bindings.
     let out_file = PathBuf::from(env::var("OUT_DIR").unwrap()).join("bindings.rs");
     std::fs::write(out_file, bindings_source).expect("Unable to save generated Embree bindings.");
 }
